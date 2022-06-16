2f4beaf71e330267edbf4f815f9ce655249ee4b0
halid tazurkaev <barzaev97@gmail.com>
2022-06-16 09:43:50 +0300

    Исправил баг с текстом

--- src/App.js
+++ src/App.js
@@ -2,72 +2,15 @@ import React, {useEffect, useRef, useState} from 'react';
 import { Stage, Layer, Text, Transformer } from 'react-konva';
 
 import  "./App.css"
+import RenderText from "./RenderText";
 
-const RenderText = ({shapeProps, onSelect, onChange}) => {
-    const shapeRef = useRef();
-    const trRef = useRef();
-    const [isSelected, setIsSelected] = useState(false)
-    useEffect(() => {
-        if (isSelected) {
-            trRef.current.nodes([shapeRef.current]);
-            trRef.current.getLayer().batchDraw();
-        }
-    }, [isSelected]);
-    return (
-        <>
-        <Text
-            padding={5}
-            onDblClick={() =>setIsSelected(!isSelected)}
-            text={shapeProps.text}
-            onClick={onSelect}
-            ref={shapeRef}
-            draggable
-            onDragEnd={(e) => {
-
-                onChange({
-                    ...shapeProps,
-                    x: e.target.x(),
-                    y: e.target.y(),
-                });
-            }}
-            onTransformEnd={(e) => {
-                const node = shapeRef.current;
-                const scaleX = node.scaleX();
-                const scaleY = node.scaleY();
-
-                node.scaleX();
-                node.scaleY();
-
-                onChange({
-                    ...shapeProps,
-                    x: node.x(),
-                    y: node.y(),
-                    width: Math.max(5, node.width() * scaleX),
-                    height: Math.max(node.height() * scaleY),
-                });
-            }}
-        />
-    {isSelected && (
-        <Transformer
-            ref={trRef}
-            boundBoxFunc={(oldBox, newBox) => {
-                if (newBox.width < 5 || newBox.height < 5) {
-                    return oldBox;
-                }
-                return newBox;
-            }}
-        />
-    )}
-    </>
-    )
-}
 const App = () => {
 
     const text1 = {
         x: 50,
         y: 100,
         fontSize: 30,
-        text: '"Вывод кликни 2 раза"',
+        text: '"Кликни 2 раза"',
         draggable: true,
     }
     const [text, setText] = useState(text1)
---
+++ src/RenderText.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import {useEffect, useRef, useState} from "react";
+import {Text, Transformer} from "react-konva";
+
+const RenderText = ({shapeProps, onSelect, onChange}) => {
+    const shapeRef = useRef();
+    const trRef = useRef();
+    const [isSelected, setIsSelected] = useState(false)
+
+    useEffect(() => {
+        if(shapeProps.text.length === 0) {
+            setIsSelected(false)
+        }
+
+        if (isSelected) {
+            trRef.current.nodes([shapeRef.current]);
+            trRef.current.getLayer().batchDraw();
+        }
+    }, [isSelected, shapeProps]);
+
+
+    return (
+        <>
+            <Text
+                padding={5}
+                onDblClick={() =>setIsSelected(!isSelected)}
+                text={shapeProps.text}
+                onClick={onSelect}
+                ref={shapeRef}
+                draggable
+                onDragEnd={(e) => {
+
+                    onChange({
+                        ...shapeProps,
+                        x: e.target.x(),
+                        y: e.target.y(),
+                    });
+                }}
+                onTransformEnd={(e) => {
+                    const node = shapeRef.current;
+                    const scaleX = node.scaleX();
+                    const scaleY = node.scaleY();
+
+                    node.scaleX();
+                    node.scaleY();
+
+                    onChange({
+                        ...shapeProps,
+                        x: node.x(),
+                        y: node.y(),
+                        width: Math.max(5, node.width() * scaleX),
+                        height: Math.max(node.height() * scaleY),
+                    });
+                }}
+            />
+            {isSelected && (
+                <Transformer
+                    ref={trRef}
+                    boundBoxFunc={(oldBox, newBox) => {
+                        if (newBox.width < 5 || newBox.height < 5) {
+                            return oldBox;
+                        }
+                        return newBox;
+                    }}
+                />
+            )}
+        </>
+    )
+};
+
+export default RenderText;
